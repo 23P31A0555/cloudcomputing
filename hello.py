c7fc520a60d5c3f1b4879b626b9d11fc577dc22e
S.Jahnavi <147792372+23P31A0555@users.noreply.github.com>
2025-12-27 12:18:31 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask
-app=Flask(__name__)
-@app.route('/hello',method=['GET'])
+app = Flask(__name__)
+@app.route('/hello', methods=['GET'])
 def hello():
-    return "Hello CSE"
-if __name__=='__main__':
-    app.run(debug=True)
+    return "Hello from Flask!
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
